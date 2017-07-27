3442b95b576df228d5e85385dab25d14c6fbb9dc
stefbraun <stefan.sb.braun@outlook.com>
2017-07-27 17:59:59 +0200

    Added iterator based on pytables

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='seqtools',
-      version='0.3.1',
+      version='0.3.2',
       description='seqtools',
       url='https://github.com/stefbraun/sequence_tools',
       author='Stefan Braun',
